d790323a58f24b237effe31f5c0d88c7c63938b9
Jeremy <jrvtexas@outlook.com>
2022-10-31 15:39:28 -0500

    Teleport ability and code optimization
    I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c# I hate c#

--- Assets/Scripts/GridMovement.cs
+++ Assets/Scripts/GridMovement.cs
@@ -1,9 +1,14 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GridMovement : MonoBehaviour
 {
+
+    public Vector3 SavedMovement = new Vector3(0, 0, 0);
+    public float speed = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,32 +18,55 @@ public class GridMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int w = 0;
+        int s = 0;
+        int d = 0;
+        int a = 0;
+
         if(Input.GetKeyDown(KeyCode.W)) {
-            if (!Physics.CheckSphere(transform.position + new Vector3(0,0,1), 0.1f)) {
-                transform.position += new Vector3(0, 0, 1);
-            }        
-            
-            
+            if (!Physics.CheckSphere(transform.position + new Vector3(0, 0, 1), 0.1f))
+            {
+                w = 1;
+            }
         }
         if(Input.GetKeyDown(KeyCode.S))
         {
             if (!Physics.CheckSphere(transform.position + new Vector3(0, 0, -1), 0.1f)){
 
-                transform.position += new Vector3(0, 0, -1);
+                s = 1;
             }
         }
         if(Input.GetKeyDown(KeyCode.D)) {
             if (!Physics.CheckSphere(transform.position + new Vector3(1, 0, 0), 0.1f)){
-                transform.position += new Vector3(1, 0, 0);
+                a = 1;
             }
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
             if (!Physics.CheckSphere(transform.position + new Vector3(-1, 0, 0), 0.1f)){
-                transform.position += new Vector3(-1, 0, 0);
+                d = 1;
+            }
+
+        }
+
+
+        Vector3 pos = new Vector3(a - d, 0, w - s);
+        transform.position += pos;
+
+
+        if (pos != Vector3.zero)
+        {
+            SavedMovement = pos;
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!Physics.CheckSphere(transform.position + SavedMovement * 2, 0.2f))
+            {
+                transform.position += SavedMovement * 2;
+                
             }
         }
-        
 
 
 
